baca5e2558a20f8e6cdcb7a45dcc464cff572811
abeersaxena2006 <abeersaxena448@gmail.com>
2026-05-09 21:59:35 +0530

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const schoolRoutes = require('./routes/schoolRoutes');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Health check
+app.get('/', (req, res) => {
+  res.json({ status: 'ok', message: 'School Management API is running' });
+});
+
+// Routes
+app.use('/', schoolRoutes);
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).json({ success: false, message: 'Route not found' });
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ success: false, message: 'Internal server error' });
+});
+
+module.exports = app;
